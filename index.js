bf114bf3bff58ee4925012967d809fcd7142b138
BongominErickJuma <ericbongomin@gmail.com>
2025-01-16 22:20:11 +0300

    created tables

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ import performance_app from "./statistics/performances/Sperformance/sperformance
 import answered_app from "./statistics/answered/answered.js";
 import password_app from "./updates/passwords.js";
 import images_app from "./updates/images.js";
-import dropTables from "./database/dropTables.js";
-// import createTables from "./database/createTables.js";
+// import dropTables from "./database/dropTables.js";
+import createTables from "./database/createTables.js";
 // import createAdmin from "./database/createAdmin.js";
 
 // configure express
@@ -35,10 +35,10 @@ app.use(
 );
 
 // Drop all The tables
-dropTables();
+// dropTables();
 
 // Table Creation
-// createTables();
+createTables();
 
 // Admin Creation
 // createAdmin();
